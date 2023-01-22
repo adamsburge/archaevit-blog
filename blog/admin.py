95f1fe0849b16757acbd36447c97d42042a9bebf
adamsburge <adam.scott.burge@gmail.com>
2023-01-22 16:02:22 +0000

    Create and migrate databases and create superuser

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import NewUser
 
 # Register your models here.
+fields = list(UserAdmin.fieldsets)
+fields[1] = ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'institution', 'role')})
+UserAdmin.fieldsets = tuple(fields)
+
+admin.site.register(NewUser, UserAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class NewUser(AbstractUser):
+    institution = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    site_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(NewUser, on_delete=models.CASCADE, related_name="blog_post")
+    updated_on = models.DateTimeField(auto_now=True)
+    country = models.CharField(max_length=50)
+    dates = models.CharField(max_length=50)
+    description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    interesting_likes = models.ManyToManyField(NewUser, related_name='blog_interesting_likes', blank=True)
+    important_likes = models.ManyToManyField(NewUser, related_name='blog_important_likes', blank=True)
+    underrated_likes = models.ManyToManyField(NewUser, related_name='blog_underrated_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_interesting_likes(self):
+        return self.interesting_likes.count()
+
+    def number_of_important_likes(self):
+        return self.important_likes.count()
+
+    def number_of_underrated_likes(self):
+        return self.underrated_likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
