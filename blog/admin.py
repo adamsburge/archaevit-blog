e54668770f66f1aa0f0b4681502e23f47b384285
adamsburge <adam.scott.burge@gmail.com>
2023-01-23 13:44:04 +0000

    Add search and filter abilities to admin panel and add ability to approve comments

--- blog/admin.py
+++ blog/admin.py
@@ -6,10 +6,25 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'status', 'country', 'dates', 'created_on')
+    search_fields = ['title', 'description', 'country', 'dates']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
 
-admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
 
 fields = list(UserAdmin.fieldsets)
 fields[1] = ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'institution', 'role')})
